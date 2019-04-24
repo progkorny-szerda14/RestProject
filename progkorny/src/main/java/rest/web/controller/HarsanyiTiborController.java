53fb7c9fe6193ddb26a34c3559d39b4480604b43
Tibor <teborrr@gmail.com>
2019-04-24 14:25:30 +0200

    Asd

---
+++ progkorny/src/main/java/rest/web/controller/HarsanyiTiborController.java
@@ -0,0 +1,24 @@
+package rest.web.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rest.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ht")
+@Slf4j
+public class SzaboMateController {
+
+    @GetMapping("")
+    public User getUser() {
+        log.info("process=get-users");
+
+        return new User("Harsanyi Tibor", "teborrr@gmail.com", LocalDateTime.now() , LocalDateTime.now());
+    }
+}
